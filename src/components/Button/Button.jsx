2ce6c712ac60c762660f345d97916def65582699
dariameln1 <dariamelnykova1@gmail.com>
2023-12-15 00:10:46 +0100

    updated

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,8 @@
 import { Btn } from './Button.styled';
-import { BtnWrapper } from './Button.styled'
 
 const Button = ({ onLoadMore, isSearchPerformed }) => {
   return isSearchPerformed ? (
-    <BtnWrapper>
       <Btn onClick={onLoadMore}>Load more</Btn>
-    </BtnWrapper>
   ) : null;
     
 };
--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -1,30 +1,22 @@
 import styled from 'styled-components';
 
-export const BtnWrapper = styled.div`
-  position: absolute; 
-  bottom: 20px; 
-  left: 50%; 
-  transform: translateX(-50%);
-  z-index: -1;
-`
-
 export const Btn = styled.button`
-  margin-left: auto;
-  margin-right: auto;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
   padding: 8px 16px;
-  border-radius: 2px;
+  border-radius: 8px;
   background-color: #4682B4;
-  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
-  text-align: center;
-  display: flex;
-  justify-content: center;
+  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
   color: #fff;
   border: 0;
   text-decoration: none;
   cursor: pointer;
+  font-family: inherit;
   font-size: 18px;
   line-height: 24px;
-  font-weight: 500;
+  font-style: normal;
+  font-weight: 500;  
   min-width: 180px;
   box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
     0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
